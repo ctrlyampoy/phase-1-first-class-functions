3d518b7798c5bcee0bab4b507e0b8e9488cdec6e
Yampoy <yampoy.elvis@gmail.com>
2025-03-08 22:40:58 +0300

    Done with callback

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    function namedFunction() {
+        return 'I am a named function!';
+    }
+    return namedFunction;
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        return 'I am an anonymous function!';
+    };
+}
